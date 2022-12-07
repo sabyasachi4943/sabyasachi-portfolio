e97d696f7592dd6cccdfe66e21c5fdb333f118ae
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-12-07 21:34:46 +0600

    apos

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ export default function Home() {
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
               Freelancer providing services for programming and design content
-              needs. Join me down below and let's get cracking!
+              needs. Join me down below and let&apos;s get cracking!
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               <a
